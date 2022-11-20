539ac62e4e46d039c8e05260be1aef185d70908d
KateOvchinikova <e23111999@gmail.com>
2022-11-20 21:09:53 +0200

    Finaly

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("#body"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,6 +11,11 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+  }
+  refs.openModalBtn.addEventListener("click", toggleBody);
+  refs.closeModalBtn.addEventListener("click", toggleBody);
+  function toggleBody() {
+    refs.body.classList.toggle("lock");
   }
 })();
 (() => {
